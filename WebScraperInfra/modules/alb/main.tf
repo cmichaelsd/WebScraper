7a309671938ae8fbf225365945c099cec614bf49
Cole Michaels <cmichaelsd@gmail.com>
2026-02-25 11:31:46 +0900

    added web application firewall.

--- WebScraperInfra/modules/alb/main.tf
+++ WebScraperInfra/modules/alb/main.tf
@@ -34,3 +34,46 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.api.arn
   }
 }
+
+resource "aws_wafv2_web_acl" "api" {
+  name = "${var.project_name}-waf"
+  scope = "REGIONAL"
+  description = "Rate limiting for API"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name = "rate-limit"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit = 50
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name = "rateLimit"
+      sampled_requests_enabled = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name = "${var.project_name}-waf"
+    sampled_requests_enabled = true
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "api" {
+  resource_arn = aws_lb.this.arn
+  web_acl_arn = aws_wafv2_web_acl.api.arn
+}
